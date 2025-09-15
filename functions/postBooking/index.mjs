9a1d67b0b2e31396eebb8ed82b2d21de2bb8d8db
Lisa-Ni <niklasson.lisa@gmail.com>
2025-09-15 12:10:53 +0200

    basic PostBooking

--- functions/postBooking/index.mjs
+++ functions/postBooking/index.mjs
@@ -1,28 +1,46 @@
-import { PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { client } from "../../services/db.mjs";
-import { v4 as uuidv4 } from "uuid";
+import { PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { client } from '../../services/db.mjs';
+import { v4 as uuidv4 } from 'uuid';
+
+// const maxRooms = 20;
 
 export const handler = async (event) => {
-  const id_unsorted = uuidv4();
-  const id = id_unsorted.slice(0, 5);
-  /*const booking = JSON.parse(event.body);*/
+	//genererar ett id
+	const id_unsorted = uuidv4();
+	const id = id_unsorted.slice(0, 5);
 
-  const command = new PutItemCommand({
-    TableName: "BonzAIDataTable",
-    Item: {
-      pk: { S: "booking" },
-      sk: { S: id },
-    }
-  });
+	//hämtar vad användaren skrivit i body
+	const booking = JSON.parse(event.body);
 
-  await client.send(command);
+	// Gör en ny booking
+	const command = new PutItemCommand({
+		TableName: 'BonzAIDataTable',
+		Item: {
+			pk: { S: 'booking' },
+			sk: { S: String(id) },
+			guests: { N: booking.guests.toString() }, //number > string
+			rooms: { S: JSON.stringify(booking.rooms) }, //array > string
+			dateIN: { S: booking.dateIN },
+			dateOUT: { S: booking.dateOUT },
+			name: { S: booking.name },
+			mail: { S: booking.mail },
+		},
+	});
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      message: "Booking booked, read a book!",
-    }),
-  };
+	await client.send(command);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			success: true,
+			message: 'Booking Created',
+			bookingId: id,
+			name: booking.name,
+			guests: booking.guests,
+			rooms: booking.rooms,
+			// total,
+			dateIN: booking.dateIN,
+			dateOUT: booking.dateOUT,
+		}),
+	};
 };
-
