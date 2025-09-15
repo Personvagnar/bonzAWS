d713d35601fd6ddf2cf40a3d82214fdfe531d355
Senja Erlandsson <senjaerlandsson@gmail.com>
2025-09-15 13:24:15 +0200

    updated getBookings

--- functions/getBookings/index.mjs
+++ functions/getBookings/index.mjs
@@ -1,25 +1,27 @@
-import { confirmation } from "../../services/middleware/confirm.js";
 import { client } from "../../services/db.mjs";
 import { ScanCommand } from "@aws-sdk/client-dynamodb";
 
 export const handler = async (event) => {
   try {
     const command = new ScanCommand({
-      tableName: "BonzAIDataTable"
+      TableName: "BonzAIDataTable"
     });
 
     const data = await client.send(command)
 
-    const bookings = data.Items.map(item => ({
-      id: item.id.S,
-      guests: parseInt(item.guests.N),
-      rooms: JSON.parse(item.rooms.S),
-      dateIN: item.dateIN.S,
-      dateOUT: item.dateOUT.S,
-      name: item.name.S,
-      mail: item.mail.S
-    }))
-    
+    const bookings = data.Items
+      .filter(item => item.sk && item.guests && item.rooms && item.dateIN && item.dateOUT && item.name)
+      .map(item => ({
+        bookingId: item.sk.S,
+        guests: parseInt(item.guests.N),
+        rooms: JSON.parse(item.rooms.S),
+        dateIN: item.dateIN.S,
+        dateOUT: item.dateOUT.S,
+        name: item.name.S,
+        mail: item.mail?.S || ""
+      }));
+
+
     return {
       statusCode: 200,
       body: JSON.stringify(bookings)
@@ -28,7 +30,7 @@ export const handler = async (event) => {
     console.error(err);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Failed to retrieve booked bookings book"}),
+      body: JSON.stringify({ error: "Failed to retrieve booked bookings book" }),
     }
   }
 };
