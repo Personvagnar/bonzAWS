444f62b00456c048336343cb2300e1e46fc89f9c
Senja Erlandsson <senjaerlandsson@gmail.com>
2025-09-15 12:30:18 +0200

    feat: added GET endpoint for bookings

--- functions/getBookings/index.mjs
+++ functions/getBookings/index.mjs
@@ -1,10 +1,34 @@
 import { confirmation } from "../../services/middleware/confirm.js";
+import { client } from "../../services/db.mjs";
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
 
 export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+  try {
+    const command = new ScanCommand({
+      tableName: "BonzAIDataTable"
+    });
+
+    const data = await client.send(command)
+
+    const bookings = data.Items.map(item => ({
+      id: item.id.S,
+      guests: parseInt(item.guests.N),
+      rooms: JSON.parse(item.rooms.S),
+      dateIN: item.dateIN.S,
+      dateOUT: item.dateOUT.S,
+      name: item.name.S,
+      mail: item.mail.S
+    }))
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(bookings)
+    }
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to retrieve booked bookings book"}),
+    }
+  }
 };
